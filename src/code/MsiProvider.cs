35e207e84f7cd19b809a1a20119c5fa016a18cc7
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2024-12-05 20:50:03 -0600

    Add Find-Package

--- src/code/MsiProvider.cs
+++ src/code/MsiProvider.cs
@@ -2,10 +2,89 @@
 // You may use, distribute and modify this code under the
 // terms of the MIT license.
 
+using WixToolset.Dtf.WindowsInstaller;
+
 namespace AnyPackage.Provider.Msi;
 
-[PackageProvider("Msi", FileExtensions = [".msi", ".msu"])]
-public class MsiProvider : PackageProvider
+[PackageProvider("Msi", PackageByName = false, FileExtensions = [".msi", ".msp"])]
+public class MsiProvider : PackageProvider, IFindPackage
 {
+    public void FindPackage(PackageRequest request)
+    {
+        PackageInfo package;
 
+        if (Path.GetExtension(request.Path) == ".msi")
+        {
+            package = FindPackageMsi(request);
+        }
+        else
+        {
+            package = FindPackageMsp(request);
+        }
+
+        request.WritePackage(package);
+    }
+
+    private PackageInfo FindPackageMsi(PackageRequest request)
+    {
+        using var database = new Database(request.Path, DatabaseOpenMode.ReadOnly);
+        var metadata = GetMetadataMsi(database);
+
+        var source = new PackageSourceInfo(request.Path, request.Path, ProviderInfo);
+        var package = new PackageInfo(metadata["ProductName"]!.ToString(),
+                                      metadata["ProductVersion"]!.ToString(),
+                                      source,
+                                      description: "",
+                                      dependencies: null,
+                                      metadata,
+                                      ProviderInfo);
+
+        return package;
+    }
+
+    private PackageInfo FindPackageMsp(PackageRequest request)
+    {
+        using var database = new Database(request.Path, DatabaseOpenMode.ReadOnly);
+        var metadata = GetMetadataMsp(database);
+
+        var source = new PackageSourceInfo(request.Path, request.Path, ProviderInfo);
+        var package = new PackageInfo(metadata["DisplayName"]!.ToString(),
+                                      version: null,
+                                      source,
+                                      description: metadata["Description"]!.ToString(),
+                                      dependencies: null,
+                                      metadata,
+                                      ProviderInfo);
+
+        return package;
+    }
+
+    private static Dictionary<string, object?> GetMetadataMsi(Database database)
+    {
+        var props = database.ExecuteStringQuery("SELECT Property FROM Property");
+        var metadata = new Dictionary<string, object?>();
+
+        foreach (var prop in props)
+        {
+            var value = database.ExecutePropertyQuery(prop);
+            metadata.Add(prop, value);
+        }
+
+        return metadata;
+    }
+
+    private static Dictionary<string, object?> GetMetadataMsp(Database database)
+    {
+        var props = database.ExecuteStringQuery("SELECT Property FROM MsiPatchMetadata");
+        var metadata = new Dictionary<string, object?>();
+
+        foreach (var prop in props)
+        {
+            var query = $"SELECT Value FROM MsiPatchMetadata WHERE Property = '{prop}'";
+            var value = database.ExecuteStringQuery(query);
+            metadata.Add(prop, string.Join(Environment.NewLine, value));
+        }
+
+        return metadata;
+    }
 }
